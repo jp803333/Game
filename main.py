cd088e4627fbd86cc1d2c6a81d60d2f8ebb248a1
Jitendra Prajapati <jp803333@gmail.com>
2019-01-19 23:14:59 +0530

    test3

--- main.py
+++ main.py
@@ -11,22 +11,50 @@ sts = 0
 bgimage = pygame.image.load('resources\main.png')
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('craap')
-screen.blit(bgimage, (0, 0))
 
 while True:
     if sts == 0:
+        screen.blit(bgimage, (0, 0))
+        newbtn1 = btn(250, 300, 100, 50, screen, (0, 0, 200), 'New Game', 16)
+        newbtn1.draw()
         mouse = pygame.mouse.get_pos()
-        if 350 >= mouse[0] > 250 and 450 >= mouse[1] > 400:
-            newbtn = btn(250, 400, 100, 50, screen, (0, 0, 255), 'New Game',16)
-            newbtn.draw()
+        if 350 >= mouse[0] > 250 and 350 >= mouse[1] > 300:
+            newbtn1 = btn(250, 300, 100, 50, screen, (0, 0, 255), 'New Game', 16)
+            newbtn1.draw()
             if pygame.mouse.get_pressed()[0]:
                 sts = 1
-                screen.fill((255, 255, 255))
-                newgame = startgame.board(screen)
-                newgame.createboard()
         else:
-            newbtn = btn(250, 400, 100, 50, screen, (0, 0, 200), 'New Game',16)
+            newbtn1 = btn(250, 300, 100, 50, screen, (0, 0, 200), 'New Game', 16)
+            newbtn1.draw()
+
+        if 350 >= mouse[0] > 250 and 450 >= mouse[1] > 400:
+            newbtn1 = btn(250, 400, 100, 50, screen, (0, 0, 255), 'Exit', 16)
+            newbtn1.draw()
+            if pygame.mouse.get_pressed()[0]:
+                pygame.quit()
+                sys.exit()
+        else:
+            newbtn1 = btn(250, 400, 100, 50, screen, (0, 0, 200), 'Exit', 16)
+            newbtn1.draw()
+
+    if sts == 1:
+        screen.fill((255, 255, 255))
+        newgame = startgame.board(screen)
+        newgame.createboard()
+        newbtn = btn(275, 12, 50, 25, screen, (0, 0, 255), 'Quit', 14)
+        newbtn.draw()
+        mouse = pygame.mouse.get_pos()
+        print(mouse)
+        if 325 >= mouse[0] > 275 and 37 >= mouse[1] > 12:
+            newbtn = btn(275, 12, 50, 25, screen, (0, 0, 255), 'Quit', 14)
             newbtn.draw()
+            if pygame.mouse.get_pressed()[0]:
+                sts = 0
+        else:
+            newbtn = btn(275, 12, 50, 25, screen, (0, 0, 200), 'Quit', 14)
+            newbtn.draw()
+
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
--- startgame.py
+++ startgame.py
@@ -1,12 +1,7 @@
 import pygame, sys
-from shapes import buttons as btn
-
 class board:
     def __init__(self, surface):
         self.surface = surface
-        pass
     def createboard(self):
-        boardimage = pygame.image.load('resources\Board.png')
-        self.surface.blit(boardimage, (50, 50 ))
-        button = btn(275, 12, 50, 25, self.surface, (0, 0, 255),'Quit',14)
-        button.draw()
+        boardimage = pygame.image.load('resources\Ludo_board.jpg')
+        self.surface.blit(boardimage, (50, 50))
