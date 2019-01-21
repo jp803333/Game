1f3ed11d9467b811caa046f729b5009ca2bfbcb3
Jitendra Prajapati <jp803333@gmail.com>
2019-01-21 14:01:21 +0530

    buttons updated

--- main.py
+++ main.py
@@ -1,63 +1,49 @@
 import pygame
-from pygame.draw import  circle as cir
 import sys
-from shapes import buttons as btn
 import startgame
-import tokens
-
+import buttons
+import random
 
 pygame.init()
 
 size = width, height = 600, 600
 sts = 0
+draw = 0
 bgimage = pygame.image.load('resources\main.png')
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('craap')
+def newgame():
+     global  sts
+     sts = 1
+def exit():
+    pygame.quit()
+    sys.exit()
+def quitgame():
+    global sts
+    sts = 0
+def Throw():
+    global draw
+    draw = random.randint(1,6)
+pcolor = [46, 64, 83]
+acolor = [52, 73, 94]
+newbtn = buttons.Buttons(250, 300, 100, 50, screen, acolor, pcolor, 'New Game', 16, newgame)
+exitbtn = buttons.Buttons(250, 400, 100, 50, screen, acolor, pcolor, 'Exit', 16, exit)
+quitbtn = buttons.Buttons(400, 10, 100, 30, screen, acolor, pcolor, 'Quit Game', 14, quitgame)
+dicebtn = buttons.Buttons(100, 10, 100, 30, screen, acolor, pcolor, 'Throw Dice', 14, Throw)
 
 while True:
     if sts == 0:
         screen.blit(bgimage, (0, 0))
-        newbtn1 = btn(250, 300, 100, 50, screen, (0, 0, 200), 'New Game', 16)
-        newbtn1.draw()
-        mouse = pygame.mouse.get_pos()
-        if 350 >= mouse[0] > 250 and 350 >= mouse[1] > 300:
-            newbtn1 = btn(250, 300, 100, 50, screen, (0, 0, 255), 'New Game', 16)
-            newbtn1.draw()
-            if pygame.mouse.get_pressed()[0]:
-                sts = 1
-        else:
-            newbtn1 = btn(250, 300, 100, 50, screen, (0, 0, 200), 'New Game', 16)
-            newbtn1.draw()
-
-        if 350 >= mouse[0] > 250 and 450 >= mouse[1] > 400:
-            newbtn1 = btn(250, 400, 100, 50, screen, (0, 0, 255), 'Exit', 16)
-            newbtn1.draw()
-            if pygame.mouse.get_pressed()[0]:
-                pygame.quit()
-                sys.exit()
-        else:
-            newbtn1 = btn(250, 400, 100, 50, screen, (0, 0, 200), 'Exit', 16)
-            newbtn1.draw()
+        newbtn.Draw()
+        exitbtn.Draw()
 
     if sts == 1:
         screen.fill((255, 255, 255))
         newgame = startgame.board(screen)
         newgame.createboard()
-        newbtn = btn(275, 12, 50, 25, screen, (0, 0, 255), 'Quit', 14)
-        newbtn.draw()
-        mouse = pygame.mouse.get_pos()
-        if 325 >= mouse[0] > 275 and 37 >= mouse[1] > 12:
-            newbtn = btn(275, 12, 50, 25, screen, (0, 0, 255), 'Quit', 14)
-            newbtn.draw()
-            if pygame.mouse.get_pressed()[0]:
-                sts = 0
-        else:
-            newbtn = btn(275, 12, 50, 25, screen, (0, 0, 200), 'Quit', 14)
-            newbtn.draw()
-        red = (tokens.token(185, 435, screen, 1),tokens.token(151, 402, screen, 1), tokens.token(121, 434, screen, 1), tokens.token(151, 471, screen, 1))
-        redcor = ([190,440], [156,407], [126,439],  [156,476], )
-        for i in range(4):
-            red[i].draw()
+        quitbtn.Draw()
+        dicebtn.Draw()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
---
+++ player.py
@@ -0,0 +1,6 @@
+import pygame
+import tokens
+
+class player:
+    def __init__(self, color, surface, ):
+        pass
